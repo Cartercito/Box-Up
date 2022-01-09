560a4494c65794629668ecb82349895e94bb9a23
Cartercito <74151952+Cartercito@users.noreply.github.com>
2022-01-08 23:51:21 -0500

    Initial commit

---
+++ Assets/PlayerCatch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCatch : MonoBehaviour
+{
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+       // collision.transform.position = this.transform.position;
+        collision.transform.SetParent(transform);
+        Destroy(collision.GetComponent<Rigidbody2D>());
+        Destroy(collision);
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 screenPos = Input.mousePosition;
+        if (screenPos.x < Screen.width && screenPos.x > 0 && screenPos.y < Screen.height && screenPos.y > 0)
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(screenPos);
+            pos.y = this.transform.position.y;
+            this.transform.position = pos;
+        }
+        
+    }
+}
